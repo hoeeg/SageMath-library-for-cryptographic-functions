2e94b2ede8f9223f90861d98ff8d87fad80491e0
Mille Hoeg <pik018@uib.no>
2026-05-26 11:15:38 +0200

    fix: update method names

--- cryptographicFunctionsLibrary/__init__.py
+++ cryptographicFunctionsLibrary/__init__.py
@@ -1,8 +1,8 @@
-from .conversions import polynomial_to_matrix, matrix_to_polynomial, polynomial_to_sequence, sequence_to_polynomial, polynomial_to_truth_table, truth_table_to_polynomial
+from .conversions import univariate_to_matrix, matrix_to_univariate, univariate_to_sequence, sequence_to_univariate, univariate_to_truth_table, truth_table_to_univariate
 from .families import family_1, family_2, family_3, family_4, family_5, family_6, family_7_9, family_11, family_12, family_13
 from .membership import membership_all, membership_family_1, membership_family_2, membership_family_3, membership_family_4, membership_family_5, membership_family_6, membership_family_7_9, membership_family_11, membership_family_12, membership_family_13
 
-__convert__ = ['polynomial_to_matrix', 'matrix_to_polynomial', 'polynomial_to_sequence', 'sequence_to_polynomial', 'polynomial_to_truth_table', 'truth_table_to_polynomial']
+__convert__ = ['univariate_to_matrix', 'matrix_to_univariate', 'univariate_to_sequence', 'sequence_to_univariate', 'univariate_to_truth_table', 'truth_table_to_univariate']
 __families__ = ['family_1', 'family_2', 'family_3', 'family_4', 'family_5', 'family_6', 'family_7_9', 'family_11', 'family_12', 'family_13']
 __membership__ = ['membership_all', 'membership_family_1', 'membership_family_2', 'membership_family_3', 'membership_family_4', 'membership_family_5', 'membership_family_6', 'membership_family_7_9', 'membership_family_11', 'membership_family_12', 'membership_family_13']
 
