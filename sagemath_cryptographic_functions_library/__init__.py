734a4718f3935a31f64266a9e7b24fea7b095cae
Mille Hoeg <pik018@uib.no>
2026-05-27 23:20:49 +0200

    fix: update build

--- sagemath_cryptographic_functions_library/__init__.py
+++ sagemath_cryptographic_functions_library/__init__.py
@@ -1,5 +1,3 @@
 from .conversions import (univariate_to_matrix, matrix_to_univariate, univariate_to_sequence, sequence_to_univariate, univariate_to_truth_table, truth_table_to_univariate)
 from .families import (family_1, family_2, family_3, family_4, family_5, family_6, family_7_9, family_11, family_12, family_13)
 from .membership import (membership_all, membership_family_1, membership_family_2, membership_family_3, membership_family_4, membership_family_5, membership_family_6, membership_family_7_9, membership_family_11, membership_family_12, membership_family_13)
-
-__version__ = "0.1.0"
